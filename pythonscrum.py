6277ae0173ca82a98cd98ef81ee65b814f0806d3
gearsof44 <guillaume@xcode82-ci.local>
2017-01-12 10:52:00 +0100

    Python 12/01
    Python de fin de scrum permet de récupérer certaines infos des user
    stories

---
+++ pythonscrum.py
@@ -0,0 +1,38 @@
+from taiga import TaigaAPI
+
+api = TaigaAPI()
+
+api.auth(
+    username='admin',
+    password='123123'
+)
+
+#Selection du projet voulu
+new_project = api.projects.get_by_slug('admin-projettest3')
+
+import pdb
+pdb.set_trace()
+
+#Stockage dans une variable des users story (global)
+stories = api.user_stories.list(project=new_project.id)
+
+print('Liste de user story de tout les projets :')
+print(stories)
+print(' ')
+
+print('Affichage sujet de ma user story du projet projettest3 ref 23 :')
+print (new_project.get_userstory_by_ref(23).subject)
+print(' ')
+
+
+print('Affichage tags de ma user story du projet projettest3 ref 23 :')
+print (new_project.get_userstory_by_ref(23).tags)
+print(' ')
+
+print('Affichage points de ma user story du projet projettest3 ref 23 :')
+print (new_project.get_userstory_by_ref(23).points)
+print(' ')
+
+print('Affichage description de ma user story du projet projettest3 ref 23 :')
+print (new_project.get_userstory_by_ref(23).description)
+print(' ')
